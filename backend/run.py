4834847b2ca1d1932859321a8bcd65c38253a08d
Will <willchww@gmail.com>
2020-09-13 21:47:18 +0800

    [feat] db

--- backend/run.py
+++ backend/run.py
@@ -7,7 +7,8 @@ jwt.init_app(app)
 db.init_app(app)
 with app.app_context():
     from app.db.models import *
+
     reset_database()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=9527)
